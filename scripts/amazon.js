f02b03dc1f923d18531622266f9745368e94c44f
Codewithlola <codewithlola@gmail.com>
2025-06-27 22:13:49 +0100

    Upload productJS as a file in html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,4 @@
-      //first step
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}
-]
+      //first step is creating the Array & objects like const product= [{}]. but we already created a js file specifically for the products. we just included it in the html file and use it here.
 
       //second step
 let productsHTML = '';
